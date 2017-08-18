bc0026d7066162df139aa6257f29b163c5693ca4
kartik <chauhan.kartik25@gmail.com>
2017-08-19 01:14:42 +0530

    woked upto the rendering of active cells in memorize state

--- src/app/cell.js
+++ src/app/cell.js
@@ -2,10 +2,23 @@ import React from 'react';
 
 class Cell extends React.Component
 {
+    checkActive()
+    {
+        return this.props.activeCells.indexOf(this.props.id) > -1 ? true : false;
+    }
+
     render()
     {
+        let cellClass = "cell"; // this will be the default class for every cell present
+        /* if the current gameState is 'memorize', the following condition will check for all the active cells & append the active
+           class to those cells */
+        if(this.props.gameState == 'memorize' && this.checkActive())    
+        {
+            cellClass += " active"; // 
+        }
+
         return(
-            <div className="cell">
+            <div className={cellClass}>
                 {this.props.id}
             </div>
         );
--- src/app/game.js
+++ src/app/game.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import _ from 'lodash';
+
 import Cell from './cell';
 import Row from './row';
 import Footer from './footer';
+
 
 class Game extends React.Component
 {
@@ -11,10 +14,26 @@ class Game extends React.Component
         this.state = {
             gameState: 'ready', // this will be the initial gameState
         };
+        this.matrix = [];
+        for(let r=0; r<this.props.rows; r++)
+        {
+            let row = [];
+            for(let c=0; c<this.props.cols; c++)
+            {
+                row.push(`${r}${c}`);
+            }
+            this.matrix.push(row);
+        }
+
+        // flatten will convert the 2D matrix into a 1D array and return the array.
+        let flattenedArray = _.flatten(this.matrix, true);
+        // sampleSize will randomly pick specified number of elements from a given array and will return the new array
+        this.activeCells = _.sampleSize(flattenedArray, this.props.activeCellsCount);
     }
 
-    // Once the first render of the grid gets complete ComponentDidMount will execute and change the state of the game
-    // using setTimeOut
+    // Once the first render of the grid gets complete ComponentDidMount will execute and change the state of the game using setTimeOut
+    // Callback for 'recall' state if in case setState => 'memorize' fails.
+    // We don't want to jump to the subsequent state if a state fails to update 
     componentDidMount()
     {
         setTimeout(() => { this.setState({gameState: 'memorize'}, () => {setTimeout( () => {this.setState({gameState: 'recall'})}, 2000)}  )},  2000);
@@ -22,22 +41,11 @@ class Game extends React.Component
 
     render()
     {
-        let matrix = [], row;
-        for(let r=0; r<this.props.rows; r++)
-        {
-            row = [];
-            for(let c=0; c<this.props.cols; c++)
-            {
-                row.push(`${r}${c}`);
-            }
-            matrix.push(row);
-        }
-
         return(
             <div className="grid">
-                {matrix.map((row, ri) => (
+                {this.matrix.map((row, ri) => (
                     <Row key={ri}>
-                        {row.map(cellId => <Cell key={cellId} id={cellId} />)}
+                        {row.map(cellId => <Cell key={cellId} id={cellId} activeCells={this.activeCells} {...this.state} />)}
                     </Row>
                 ))}
                 <Footer gameState={this.state.gameState}/>
